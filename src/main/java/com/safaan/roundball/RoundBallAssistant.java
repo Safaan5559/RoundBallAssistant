cdad8952f5dbcd948559f3caaa66f20738bcbe4f
Safaan5559 <safaan865@gmail.com>
2026-08-27 09:53:19 +0530

    Add main mod entry point

---
+++ src/main/java/com/safaan/roundball/RoundBallAssistant.java
@@ -0,0 +1,16 @@
+package com.safaan.roundball;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** Main server/common entry point for Round Ball Assistant. */
+public final class RoundBallAssistant implements ModInitializer {
+    public static final String MOD_ID = "roundballassistant";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Round Ball Assistant initialized for Minecraft 1.21.1");
+    }
+}
